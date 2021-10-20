f799b8d116f5db757e13c7c7646013bb7c18e828
Aathreya Sharma <aathreya.sharma@outlook.com>
2021-10-20 23:53:53 +0530

    Make user invitable

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -8,13 +8,11 @@ consumer.subscriptions.create("RoomChannel", {
 
   disconnected() {
     // Called when the subscription has been terminated by the server
-    console.log("Dis-Connected from the room!");
-
+    console.log("Disconnected from the room!");
   },
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    console.log("Recieving:")
     console.log(data.content)
     $('#msg').append('<div class="message"> ' + data.content + '</div>')
   }
@@ -27,7 +25,7 @@ $(document).on('turbolinks:load', function () {
 submit_messages = function () {
   $('#message_content').on('keydown', function (event) {
     if (event.keyCode == 13) {
-      $('input').click()
+      $('.submit').click()
       event.target.value = ''
       event.preventDefault()
     }
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :invitable, :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :validatable, :confirmable, :trackable, :lockable
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, presence: true, uniqueness: {case_sensitive: false}
+  # belongs_to :managed_by, class_name: 'User', optional: true
+  has_many :invitations, :class_name => self.to_s, :as => :invited_by
+end
