42483471376a5738831d676220d602979aa670fe
Aathreya Sharma <aathreya.sharma@outlook.com>
2021-10-21 22:56:50 +0530

    Add logger

--- app/jobs/process_csv_upload_job.rb
+++ app/jobs/process_csv_upload_job.rb
@@ -15,15 +15,15 @@ class ProcessCsvUploadJob
           user.invited_by_id = current_user_id
           user.invited_by_type = 'User'
           user.invite!
-          Rails.logger.info "User Invited: #{user.email}"
+          logger.info "User Invited: #{user.email}"
         else
-          Rails.logger.info "User exist: #{user.email}"
+          logger.info "User exist: #{user.email}"
         end
       end
       notification = Notification.create(user_id: current_user_id, message: "Job Completed for file #{import_file.file_name}")
       ActionCable.server.broadcast "room_channel_#{current_user_id}", message: notification.message
     rescue Exception => e
-      Rails.logger.info e
+      logger.info e
     end
   end
 
